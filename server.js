54c74ad2a496a0c3ad88abda88af09dca6413c23
Chandu <shekard.chandra869@gmail.com>
2018-04-26 14:16:43 +0530

    Approver

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ var port = 3000;
 var router = express.Router();
 mongoose.connect("mongodb://localhost:27017/testDB");
 app.use(cors());
-app.use('/', express.static(__dirname + '/dist'));
+//app.use('/', express.static(__dirname + '/dist'));
 
 app.use("/restAPI",router);
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-    });
+//app.get('*',(req,res)=>{
+  //  res.sendFile(path.join(__dirname + '/dist/index.html'));
+   // });
 router.use(function(req,res,next){
 //First to recieve the request hence request logging can be done here
     next();
@@ -51,8 +51,8 @@ router.route("/user")
 
 //GET ALL USER DATA
 .get(function(req,res){
-    if(typeof(req.query.type!="undefined") && req.query.type=="findOne"){
-        model.find({"name":"ChandraShekar"},{data:0},function(err,users){
+    if(typeof(req.query.condition!="undefined")){
+        model.find(JSON.parse(req.query.condition),function(err,users){
             if(err)
             res.send(err);
             else
@@ -60,12 +60,7 @@ router.route("/user")
         });
       }
     else{
-    model.find(function(err,users){
-        if(err)
-        res.send(err);
-        else
-        res.send(users);
-    });
+        res.status(500).send("NO_USERS_FOUND");
 }
 })
 
@@ -120,7 +115,7 @@ router.route("/updateTimeSheet")
 .put(verifyToken,function(req,res){
 
     console.log("triggered update",req.body.condition);
-    model.findOne(JSON.parse(req.body.condition),function(err,data){
+    model.find(JSON.parse(req.body.condition),function(err,data){
         if(err)
         res.send(err);
         else 
@@ -145,8 +140,11 @@ router.route("/updateTimeSheet")
             }
             else{
                 var reqData=JSON.parse(req.body.data);
+                console.log(data);
+                data=data[0];
                 data["data"].some((element,index) => {
-                    if(element.ProjectName==reqData.ProjectName && element.StageName==reqData.StageName && element.Taskname==reqData.Taskname && element.Rejected==1)
+                    console.log(element.EntryForDate,reqData.EntryForDate);
+                    if(element.EntryForDate==reqData.EntryForDate && element.ProjectName==reqData.ProjectName && element.StageName==reqData.StageName && element.Taskname==reqData.Taskname && element.Rejected==1)
                     {
                         data["data"][index]["Rejected"]=0;
                         data["data"][index]["Approved"]=0;
@@ -154,6 +152,7 @@ router.route("/updateTimeSheet")
                         data["data"][index]["TaskData"]["minutes"]=reqData.TaskData.minutes;
                         data["data"][index]["TaskData"]["comments"]=reqData.TaskData.comments;
                         console.log(data["data"][index]["TaskData"]);
+                        console.log("check 1");
                         data.markModified('data');
                         data.save(function(err){
                         if(err){
@@ -166,8 +165,8 @@ router.route("/updateTimeSheet")
                     }
                     });
                     }
-                    else if(element.ProjectName==reqData.ProjectName && element.StageName==reqData.StageName && element.Taskname==reqData.Taskname && element.Rejected==0 && element.Approved!=0) {
-                        console.log("check",element.ProjectName);
+                    else if(element.EntryForDate==reqData.EntryForDate && element.ProjectName==reqData.ProjectName && element.StageName==reqData.StageName && element.Taskname==reqData.Taskname && element.Rejected==0 && element.Approved!=0) {
+                        console.log("check",element);
                         res.status(500).send("DUPLICATE_ENTRY");
                         return true;
                     }
@@ -176,6 +175,7 @@ router.route("/updateTimeSheet")
         }
         catch(err)
         {
+            console.log(err);
             res.status(500).send("ERROR");
             }
         }
@@ -217,6 +217,79 @@ router.route("/getInitData")
     });
 });
 
+router.route('/approver')
+.put(verifyToken,function(req,res){
+    model.aggregate(JSON.parse(req.body.userCondition)).exec(function(err,data){
+        switch(req.body.paramType)
+        {
+            case "accept":approver(req,res,data,1,0);
+            break;
+            case "reject":approver(req,res,data,0,1);
+            break;
+            case "acceptAll":approverForAll(req,res,data,1,0);
+            break;
+            case "rejectAll":approverForAll(req,res,data,0,1);
+            break;
+        }
+    });
+    
+});
+
+function approverForAll(req,res,data,approve,reject){
+if(data.length>=0){
+model.findById(data[0]["_id"],function(err,result){
+    if(err)
+    res.sendStatus(500).send("ERROR");
+    else{
+    data.map(function(element,i){
+    result.data[element.index]["Approved"]=approve;
+    result.data[element.index]["Rejected"]=reject;
+    console.log(   result.data[element.index]);
+//        console.log(result.data[element.index]);
+  if(i==data.length-1)
+  {
+      result.markModified("data");
+      console.log(result);
+      result.save(function(err){
+        if(err)
+        throw err;
+        else
+        res.send(result);
+      });
+  }  
+    });
+}
+});
+}
+else
+res.sendStatus(500).send("ERROR");
+}
+function approver(req,res,data,approve,reject){
+    var toUpdateApproved="data."+data[0]["index"]+".Approved";
+        var toUpdateRejected="data."+data[0]["index"]+".Rejected";
+        var obj={};
+        obj[toUpdateApproved]=0;
+        obj[toUpdateRejected]=0;
+    if(data[0].data.Approved==0 && data[0].data.Rejected==0){
+        obj[toUpdateApproved]=approve;
+        obj[toUpdateRejected]=reject;
+        model.update({"_id":data[0]["_id"]},{$set:obj},function(err,raw){
+            if(err)
+            res.sendStatus(500).send("ERROR");
+            else{
+                if(raw["ok"]==1){
+                    model.find({"department":req.body.department},function(err,allAdata){
+                        if(err)
+                        res.sendStatus(500).send("ERROR");
+                        else
+                        res.send(allAdata);
+                    });
+                }
+                else
+                res.sendStatus(500).send("ERROR");
+            }
+        });}
+}
 function verifyToken(req,res,next){
     var authHeader=req.headers["authorization"];
     const token=authHeader.split(" ")[1];
