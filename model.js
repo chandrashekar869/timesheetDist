3706b915fcad39a9794908d4636b78b612797a64
chandrashekar869 <shekard.chandra869@gmail.com>
2018-05-19 20:12:13 +0530

    Update model.js

--- model.js
+++ model.js
@@ -12,7 +12,7 @@ var UserSchema=new Schema({
 "forceLogOut":Boolean
 },
 {
-    collection:'testCollection'
+    collection:'userCollection'
 }
 );
-module.exports=mongoose.model("testCollection",UserSchema);
+module.exports=mongoose.model("userCollection",UserSchema);
